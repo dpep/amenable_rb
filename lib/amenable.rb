96d7d1df87ad4aff5efe551aa3535c5d901c9969
Daniel Pepper <pepper.daniel@gmail.com>
2021-07-05 19:13:58 -0700

    refinement support

--- lib/amenable.rb
+++ lib/amenable.rb
@@ -42,4 +42,49 @@ module Amenable
       end
     end
   end
+
+  refine Object do
+    def amenable(name)
+      # identify method and way to dynamically redefine it
+      if methods(false).include?(name)
+        fn = method(name).unbind # rebind later to support subclassing
+        definer = method(:define_singleton_method)
+      elsif private_methods(false).include?(name)
+        fn = method(name).unbind
+        definer = ->(name, &block) do
+          define_singleton_method(name, &block)
+          private_class_method(name)
+        end
+      elsif private_instance_methods(false).include?(name)
+        fn = instance_method(name)
+        definer = ->(name, &block) do
+          define_method(name, &block)
+          private(name)
+        end
+      elsif protected_instance_methods(false).include?(name)
+        fn = instance_method(name)
+        definer = ->(name, &block) do
+          define_method(name, &block)
+          protected(name)
+        end
+      elsif instance_methods(false).include?(name)
+        fn = instance_method(name)
+        definer = method(:define_method)
+      else
+        raise NoMethodError
+      end
+
+      # rebuild method with Amenable wrapper
+      definer.call(name) do |*args, &block|
+        # bind method to instance or subclass, which is now available
+        if fn.respond_to? :bind
+          fn = fn.bind(self)
+        end
+
+        Amenable.call(fn, *args, &block)
+      end
+
+      name
+    end
+  end
 end
